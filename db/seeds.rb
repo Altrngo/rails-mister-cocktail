9429692808e829fb1601f3eeb2ae127fda24417a
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-03-18 04:01:34 +0100

    index and show views creation for cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "Cleaning database..."
+# Ingredient.destroy_all
+
+# puts "Creating ingredients..."
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+# puts "Finished!"
+
+# *****************************************************************
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+puts "Parsing JSON..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+puts "Creating ingredients list..."
+
+ingredient["drinks"].each do |element|
+ingredient_name = Ingredient.new(name: element["strIngredient1"])
+
+puts "Saving ingredients #{ingredient_name.name}..."
+ingredient_name.save
+end
+
+puts "Finished!"
+
+# *****************************************************************
+
+puts "Cleaning database..."
+Cocktail.destroy_all
+
+puts "Creating Cocktails..."
+Cocktail.create(name: "Le Grand Schtroumpf")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Indien")
+
+puts "Finished!"
