bf9be2cbe4beb64dbf1a6781f062f7e0f3cd0429
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-03-19 12:44:07 +0100

    cleaning des vues noemment la vue cocktail/new

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,30 +4,27 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
-  end
-
   def new
     @cocktail = Cocktail.new
   end
-
+  
   def create
-    @cocktail = cocktail.new(cocktail_params)
+    @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
       render "new"
     end
   end
-
-  def doses
-    @dose_description = @cocktail.dose_description
+  
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   private
+
   def cocktail_params
     params.require(:cocktail).permit(:name)
   end
-
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,21 +4,41 @@ class DosesController < ApplicationController
 
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
+    @dose = Dose.new(params[:id])
   end
 
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @review = Review.new
+      render "cocktails/show"
+    end
+  end
+
+  # def destroy
+  #   @dose = Dose.find(params[:cocktail_id])
+  #   @dose.destroy
+  #   redirect_to cocktail_path(@cocktail)
+  # end
 
   def destroy
-    @dose = dose.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
-
 
   private
 
   def dose_params
-    params.require(:dose).permit(:content)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
+
+  # def dose_params
+  #   params.require(:dose).permit(:content)
+  # end
 
 end
