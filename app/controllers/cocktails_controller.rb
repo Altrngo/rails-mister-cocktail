f4494622b2394b88689eac50982b42178d73dadb
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-09-22 23:33:16 +0200

    Design upgrade

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -11,6 +11,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
+      @cocktail.photo = @cocktail.photo.blob
       redirect_to cocktail_path(@cocktail)
     else
       render "new"
@@ -25,6 +26,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
